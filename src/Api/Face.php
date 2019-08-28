e88c3f1aeb34994c8680a33def6236f106e47938
zhusaidong <635925926@qq.com>
2019-08-28 14:00:08 +0800

    optimize upload face image function

--- src/Api/Face.php
+++ src/Api/Face.php
@@ -12,6 +12,32 @@ use Zhusaidong\YskPlus\Response;
 class Face extends Api
 {
 	/**
+	 * get face image data
+	 *
+	 * @param array $faceImages
+	 *
+	 * @return array
+	 */
+	private function getFaceImageData(array $faceImages = [])
+	{
+		$faceImages = array_values($faceImages);
+		switch(count($faceImages))
+		{
+			case 0:
+				$faceImageData = [];
+				break;
+			case 1:
+				$faceImageData = ['face_img1' => $faceImages[0]];
+				break;
+			default:// >= 2
+				$faceImageData = ['face_img1' => $faceImages[0], 'face_img2' => $faceImages[1]];
+				break;
+		}
+		
+		return $faceImageData;
+	}
+	
+	/**
 	 * 添加人脸
 	 *
 	 * @param string $person_code   人员编码
@@ -24,25 +50,12 @@ class Face extends Api
 	 */
 	public function create(string $person_code, string $face_lib_code, string $person_name, array $ext_info = [], array $face_imgs = [])
 	{
-		switch(count($face_imgs))
-		{
-			case 0:
-				$otherData = [];
-				break;
-			case 1:
-				$otherData = ['face_img1' => $face_imgs[0]];
-				break;
-			case 2:
-				$otherData = ['face_img1' => $face_imgs[0], 'face_img2' => $face_imgs[1]];
-				break;
-		}
-		
 		return $this->request('/face/create', [
 			'person_code'   => $person_code,
 			'face_lib_code' => $face_lib_code,
 			'person_name'   => $person_name,
 			'ext_info'      => $ext_info,
-		], $otherData);
+		], $this->getFaceImageData($face_imgs));
 	}
 	
 	/**
@@ -86,20 +99,7 @@ class Face extends Api
 		!empty($person_name) and $data['person_name'] = $person_name;
 		!empty($ext_info) and $data['ext_info'] = $ext_info;
 		
-		switch(count($face_imgs))
-		{
-			case 0:
-				$otherData = [];
-				break;
-			case 1:
-				$otherData = ['face_img1' => $face_imgs[0]];
-				break;
-			case 2:
-				$otherData = ['face_img1' => $face_imgs[0], 'face_img2' => $face_imgs[1]];
-				break;
-		}
-		
-		return $this->request('/face/update', $data, $otherData);
+		return $this->request('/face/update', $data, $this->getFaceImageData($face_imgs));
 	}
 	
 	/**
